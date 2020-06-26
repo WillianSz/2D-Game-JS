466f712aabdb35da09a3802a978c4cc444977809
Willian Souza <willianscript@gmail.com>
2020-06-26 17:55:49 -0300

    Adicionado novos monstros

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,8 +1,8 @@
 class Personagem extends Animacao {
-  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
 
-    this.yInicial = height - this.altura;
+    this.yInicial = height - this.altura - this.variacaoY;
     this.y = this.yInicial
 
     this.velocidadeDoPulo = 0;
@@ -24,18 +24,6 @@ class Personagem extends Animacao {
   
   estaColidindo(inimigo) {
     noFill();
-    rect(
-      this.x,
-      this.y,
-      this.largura,
-      this.altura
-    );
-    rect(
-      inimigo.x,
-      inimigo.y,
-      inimigo.largura,
-      inimigo.altura
-    );
     const precisao = .7
     const colisao = collideRectRect(
       this.x,
--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 let imagemCenario;
 let imagemPersonagem;
 let imagemInimigo;
+let imagemInimigoVoador
+let imagemInimigoGrande;
 
 let cenario;
 let somDoJogo;
 let somDoPulo;
 let personagem;
 let inimigo;
+let inimigoGrande
+let inimigoVoador
 
 const matrizInimigo = [
   [0, 0],
@@ -56,11 +60,65 @@ const matrizPersonagem = [
   [440, 810],
   [660, 810],
 ]
+const matrizInimigoGrande = [
+  [0,0],
+  [400,0],
+  [800,0],
+  [1200,0],
+  [1600,0],
+  [0,400],
+  [400,400],
+  [800,400],
+  [1200, 400],
+  [1600, 400],
+  [0,800],
+  [400, 800],
+  [800, 800],
+  [1200, 800],
+  [1600, 800],
+  [0, 1200],
+  [400, 1200],
+  [800, 1200],
+  [1200, 1200],
+  [1600, 1200], 
+  [0, 1600],
+  [400, 1600],
+  [800, 1600],
+  [1200, 1600],
+  [1600, 1600],
+  [0, 2000],
+  [400, 2000],
+  [800, 2000],
+]
+const matrizInimigoVoador = [
+  [0,0],
+  [200, 0],
+  [400, 0],
+  [0, 150],
+  [200, 150],
+  [400, 150],
+  [0, 300],
+  [200, 300],
+  [400, 300],
+  [0, 450],
+  [200, 450],
+  [400, 450],
+  [0, 600],
+  [200, 600],
+  [400, 600],
+  [0, 750],
+]
+const inimigos = [
+
+
+]
 
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
   imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+  imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+  imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
   somDoPulo = loadSound('sons/somPulo.mp3');
 }
@@ -68,9 +126,16 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
-  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104);
-  frameRate(40)
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270);
+ const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52,30, 52, 52, 104, 104, 10 ,100);
+ const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 200, 100, 75, 200, 150, 10 ,1400);
+ const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width, 0, 200, 200, 400, 400, 10, 2500);
+  
+ inimigos.push(inimigo);
+ inimigos.push(inimigoVoador);
+ inimigos.push(inimigoGrande);
+
+ frameRate(40)
 //  somDoJogo.loop();
 }
 
@@ -88,12 +153,17 @@ function draw() {
   personagem.exibe();
   personagem.aplicaGravidade();
 
-  inimigo.exibe();
-  inimigo.move();
+  inimigos.forEach (inimigo => {
+    inimigo.exibe()
+    inimigo.move()
 
-  if (personagem.estaColidindo(inimigo)) {
-    console.log('colidiu')
-      noLoop();
-    }
+    if (personagem.estaColidindo(inimigo)) {
+      console.log('colidiu')
+        noLoop();
+      }
+
+  })
+
+
     
 }
